11a4992189ba26bff038cc66ab430c1a91164362
William Ayerst <williamayerst@AML0131.local>
2018-07-27 14:36:26 +0100

    splat syntax for outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "registry_id" {
-  value = "${azurerm_container_registry.registry.id}"
+  value = "${azurerm_container_registry.registry.*.id}"
 }
 
 output "login_server" {
-  value = "${azurerm_container_registry.registry.login_server}"
+  value = "${azurerm_container_registry.registry.*.login_server}"
 }
 
 output "admin_username" {
-  value = "${azurerm_container_registry.registry.admin_username}"
+  value = "${azurerm_container_registry.registry.*.admin_username}"
 }
 
 output "admin_password" {
-  value = "${azurerm_container_registry.registry.admin_password}"
+  value = "${azurerm_container_registry.registry.*.admin_password}"
 }
