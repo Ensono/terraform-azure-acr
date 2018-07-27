9af96f2c142dad078b3c9b1c8d4452c92969485c
William Ayerst <williamayerst@AML0131.local>
2018-07-27 15:46:22 +0100

    removed splat for static 'first element'  outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "registry_id" {
-  value = "${azurerm_container_registry.registry.*.id}"
+  value = "${azurerm_container_registry.registry.0.id}"
 }
 
 output "login_server" {
-  value = "${azurerm_container_registry.registry.*.login_server}"
+  value = "${azurerm_container_registry.registry.0.login_server}"
 }
 
 output "admin_username" {
-  value = "${azurerm_container_registry.registry.*.admin_username}"
+  value = "${azurerm_container_registry.registry.0.admin_username}"
 }
 
 output "admin_password" {
-  value = "${azurerm_container_registry.registry.*.admin_password}"
+  value = "${azurerm_container_registry.registry.0.admin_password}"
 }
